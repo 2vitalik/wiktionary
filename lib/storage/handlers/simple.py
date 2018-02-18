d22ff86f7e72b76dc2eeff87376fdca54c34d1de
Vitaliy Lyapota <2vitalik@gmail.com>
2018-02-18 22:20:59 +0200

    storage: Пробная реализация get/update/delete для simple-хранилища

--- lib/storage/handlers/simple.py
+++ lib/storage/handlers/simple.py
@@ -1,5 +1,81 @@
+from bisect import bisect_left
+from shutil import copy
+
+import os
+
+from lib.storage.builder import SimpleStorageBuilder
+from lib.storage.error import StorageError
 from lib.storage.handlers.base import BaseStorageHandler
+from lib.utils.io import read, write
+
+
+class SimpleBlock:
+    def __init__(self, path, title, handler):
+        self.path = path
+        self.title = title
+        self.handler = handler
+        block_content = read(self.path)
+        if not block_content:
+            raise StorageError(f'Block is empty: "{self.path}"')
+        self.contents = block_content.split('\n')
+        self.titles = [line.split('\t')[0] for line in self.contents]
+        try:
+            self.index = self.titles.index(title)
+        except ValueError:
+            self.index = None
+
+    def get(self):
+        if self.index is None:
+            raise StorageError(f"Block doesn't contain title: '{self.title}'")
+        return self.contents[self.index]
+
+    def delete(self):
+        if self.index is None:
+            raise StorageError(f"Block doesn't contain title: '{self.title}'")
+        del self.titles[self.index]
+        del self.contents[self.index]
+        self.save()
+
+    def update(self, value):
+        new_value = f"{self.title}\t{value}"
+        if self.index is None:  # info: случай добавления нового элемента
+            self.index = bisect_left(self.titles, self.title)
+            self.titles.insert(self.index, self.title)
+            self.contents.insert(self.index, new_value)
+        else:
+            self.contents[self.index] = new_value
+
+        self.save()
+        if len(self.titles) > self.handler.max_count:
+            self.split_block()
+
+    def save(self):
+        copy(self.path, f'{self.path}.bak')
+        write(self.path, '\n'.join(self.contents))
+
+    def get_data_dict(self):
+        data_dict = {}
+        for line in self.contents:
+            title, data = line.split('\t', maxsplit=1)
+            data_dict[title] = data
+        return data_dict
+
+    def split_block(self):
+        os.rename(self.path, f'{self.path}.old')
+        os.mkdir(self.path)
+        SplitSimpleStorageBuilder(self.handler.path, self.titles,
+                                  self.handler.max_count, splitting=True,
+                                  data_dict=self.get_data_dict())
+
+
+class SplitSimpleStorageBuilder(SimpleStorageBuilder):
+    def __init__(self, *args, data_dict=None, **kwargs):
+        self.data_dict = data_dict
+        super().__init__(*args, **kwargs)
+
+    def data(self, title):
+        return self.data_dict[title]
 
 
 class SimpleStorageHandler(BaseStorageHandler):
-    pass  # todo
+    block_class = SimpleBlock
