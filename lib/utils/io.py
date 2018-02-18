34b622249b85a1684b243d1d04a2ac81ca8b7989
Vitaliy Lyapota <2vitalik@gmail.com>
2018-02-18 18:42:48 +0200

    lib: `io`: Добавление полезных функций работы с файловой системой

--- lib/utils/io.py
+++ lib/utils/io.py
@@ -1,3 +1,5 @@
+import os
+from os.path import dirname, exists
 
 
 def read(filename):
@@ -6,5 +8,20 @@ def read(filename):
 
 
 def write(filename, content):
+    ensure_parent_dir(filename)
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
+
+
+def append(filename, line):
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(f'{line}\n')
+
+
+def ensure_dir(path):
+    if not exists(path):
+        os.makedirs(path)
+
+
+def ensure_parent_dir(filename):
+    ensure_dir(dirname(filename))
