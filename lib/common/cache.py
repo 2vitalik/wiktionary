fbd46cd32dae3af312563f75e1c0288b485f880c
Vitaliy Lyapota <2vitalik@gmail.com>
2018-02-18 13:54:09 +0200

    utils: `cache`: Класс для кеширования больших данных в памяти (с ограничением по количеству)

---
+++ lib/common/cache.py
@@ -0,0 +1,18 @@
+
+
+class Cache:
+    def __init__(self, cache_size):
+        self.keys = []
+        self.cache = {}
+        self.size = cache_size
+
+    def get(self, key):
+        if key in self.cache:
+            return self.cache[key]
+
+    def update(self, key, value):
+        if len(self.cache) > self.size:
+            remove_key = self.keys.pop(0)
+            del self.cache[remove_key]
+        self.cache[key] = value
+        self.keys.append(key)
