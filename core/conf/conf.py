8bb2eff9ba60d347d1b990e97ed27b4d1734a020
Vitaliy Lyapota <2vitalik@gmail.com>
2018-02-19 03:38:32 +0200

    conf: Исправление корневого пути

--- core/conf/conf.py
+++ core/conf/conf.py
@@ -2,7 +2,7 @@ import os
 from os.path import join
 
 
-ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 STORAGE_PATH = join(ROOT_PATH, 'storage', 'main')
 
