c5a3bee35e9bb577354d544d9b7e443c7b138ae8
Vitaliy Lyapota <2vitalik@gmail.com>
2018-07-18 02:31:18 +0300

    parse: Выделил общую часть группировщика в класс `BaseGrouper`

---
+++ lib/parse/groupers/base.py
@@ -0,0 +1,24 @@
+from lib.utils.collection import group
+
+
+class BaseGrouper:
+    fields = None
+
+    def __init__(self):
+        self._cache = {}
+
+    def values(self, *args, unique=False):
+        return self.grouped(*args, like_items=False, unique=unique)
+
+    def items(self, *args, unique=False):
+        return self.grouped(*args, like_items=True, unique=unique)
+
+    def grouped(self, *args, like_items, unique):
+        if args == ('*', ):
+            args = self.fields
+        if like_items and len(args) == len(self.fields):
+            args = args[:-1]  # because last layer will be redundant
+        if self._cache.get(args) is None:
+            indexes = [self.fields.index(arg) for arg in args]
+            self._cache[args] = group(self, indexes, like_items, unique)
+        return self._cache[args]
