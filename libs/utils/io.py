15a21f14f9aa373cafadacee9b701b4f22eef9a3
Vitaliy Lyapota <2vitalik@gmail.com>
2018-08-15 01:48:12 +0300

    io: Механизм кодирования строк в bytes для файловых операций на сервере

--- libs/utils/io.py
+++ libs/utils/io.py
@@ -2,22 +2,35 @@ import os
 from os.path import dirname, exists
 
 
+def encoded(func):
+    def wrapped(*args, **kwargs):
+        print(args)
+        if type(args[0]) == str:
+            args = (args[0].encode(), *args[1:])
+        return func(*args, **kwargs)
+    return wrapped
+
+
+@encoded
 def read(filename):
     with open(filename, encoding='utf-8') as f:
         return f.read()
 
 
+@encoded
 def write(filename, content):
     ensure_parent_dir(filename)
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
 
 
+@encoded
 def append(filename, line):
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(f'{line}\n')
 
 
+@encoded
 def ensure_dir(path):
     if not path:
         return
@@ -25,6 +38,7 @@ def ensure_dir(path):
         os.makedirs(path)
 
 
+@encoded
 def ensure_parent_dir(filename):
     ensure_dir(dirname(filename))
 
