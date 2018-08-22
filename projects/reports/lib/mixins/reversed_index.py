b8bee993484c572c5e2ed3f71d54519f9d483160
Vitaliy Lyapota <2vitalik@gmail.com>
2018-08-22 03:42:05 +0300

    reports: Упрощение кода в ReversedIndex

--- projects/reports/lib/mixins/reversed_index.py
+++ projects/reports/lib/mixins/reversed_index.py
@@ -3,7 +3,5 @@ from projects.reports.lib.base import BaseIterableReport
 
 class ReversedIndex(BaseIterableReport):
     def convert_entries(self):
-        new_entries = {}
-        for key, value in sorted(self.entries.items(), key=lambda x: x[0][::-1]):
-            new_entries[key] = value
-        self.entries = new_entries
+        self.entries = dict(sorted(self.entries.items(),
+                                   key=lambda x: x[0][::-1]))
