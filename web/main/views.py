59e0a26b9c68efc7f1eff679679c6ffe4ca6e675
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2018-08-23 17:11:08 +0300

    web: Первая простая версия веб-части (content страниц и просмотр логов)

---
+++ web/main/views.py
@@ -0,0 +1,62 @@
+import os
+from os.path import join
+
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.views import View
+
+from core.conf import conf
+from core.storage.main import storage
+from libs.parse.storage_page import StoragePage
+from libs.utils.io import read
+
+
+class PageView(View):
+    def get(self, request, *args, **kwargs):
+        page = StoragePage(kwargs['title'], silent=True)
+        return HttpResponse(f'<pre>{page.content}</pre>')
+
+
+class LogsPathMixin:
+    def get_path(self, kwargs):
+        roots = {
+            'storage': storage.logs_path,
+            # 'reports': conf.MAIN_STORAGE_PATH,
+            'errors': join(conf.LOGS_PATH, 'exceptions'),
+        }
+        root = kwargs['root']
+        path = kwargs.get('path')
+        full_path = roots[root]
+        if path:
+            full_path = join(full_path, path)
+        return root, path, full_path
+
+
+class LogsFileView(LogsPathMixin, View):
+    def get(self, request, *args, **kwargs):
+        root, path, full_path = self.get_path(kwargs)
+        content = read(full_path)
+        return HttpResponse(f'<pre>{content}</pre>')
+
+
+class LogsFolderView(LogsPathMixin, TemplateView):
+    template_name = 'logs_folder.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        root, path, full_path = self.get_path(kwargs)
+        files = {}
+        folders = {}
+        for name in sorted(os.listdir(full_path)):
+            full_name = join(full_path, name)
+            short_name = join(path, name) if path else name
+            if os.path.isdir(full_name):
+                folders[name] = short_name
+            if os.path.isfile(full_name):
+                files[name] = short_name
+        context.update({
+            'root': root,
+            'folders': folders,
+            'files': files,
+        })
+        return context
---
+++ web/project/urls.py
@@ -0,0 +1,38 @@
+"""Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from web.main.views import PageView, LogsFileView, LogsFolderView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('page/<str:title>/', PageView.as_view(),
+         name='page'),
+
+    path('logs/<str:root>/', LogsFolderView.as_view(),
+         name='logs_folder'),
+
+    path('logs/<str:root>/<path:path>/', LogsFolderView.as_view(),
+         name='logs_folder'),
+
+    path('logs/<str:root>/<path:path>', LogsFileView.as_view(),
+         name='logs_file'),
+
+    # todo: path('storage/<str:root>/<path:path>', StorageView.as_view(), name='storage'),
+]
