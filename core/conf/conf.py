8e843339fca2b803ef362beee2556096e9611692
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-01-06 23:08:04 +0200

    conf: Добавление путей хранилищ, DEBUGGING и новый трюк с "под-путями"
    `local_paths` понадобился, так как важно иметь возможность
    существующие под-пути массово изменить, изменив STORAGE_PATH в `local_conf`,
    но при этом должна быть также возможность потом индивидуально эти "под-пути" поменять.

--- core/conf/conf.py
+++ core/conf/conf.py
@@ -5,14 +5,22 @@ from os.path import join
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 STORAGE_PATH = join(ROOT_PATH, 'storage')
-
-MAIN_STORAGE_PATH = join(STORAGE_PATH, 'main')
-REPORTS_PATH = join(STORAGE_PATH, 'reports')
-SYNC_PATH = join(STORAGE_PATH, 'sync')
-
 LOGS_PATH = join(ROOT_PATH, 'logs')
+
+DEBUGGING = False
 
 try:
     from .local_conf import *
+except ImportError:
+    pass
+
+MAIN_STORAGE_PATH = join(STORAGE_PATH, 'main')
+AUTHORS_STORAGE_PATH = join(STORAGE_PATH, 'authors')
+HTMLS_STORAGE_PATH = join(STORAGE_PATH, 'htmls')
+REPORTS_PATH = join(STORAGE_PATH, 'reports')
+SYNC_PATH = join(STORAGE_PATH, 'sync')
+
+try:
+    from .local_paths import *  # we can change storage sub-paths here
 except ImportError:
     pass
