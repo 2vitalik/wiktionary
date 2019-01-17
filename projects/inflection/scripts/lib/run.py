e2e740b57e6d52f4d62e5cbb92de572a19897a45
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-01-18 00:33:12 +0200

    inflection/run: Исправление отображения таблички на псевдографике

--- projects/inflection/scripts/lib/run.py
+++ projects/inflection/scripts/lib/run.py
@@ -21,10 +21,14 @@ def run(word, index):
     len1 = 0
     len2 = 0
     for _, key1, key2 in keys:
-        len1 = max(len1, len(result[key1]))
-        len2 = max(len2, len(result[key2]))
+        len1 = max(len1, len(result.get(key1, '-')))
+        len2 = max(len2, len(result.get(key2, '-')))
 
-    width = len1 + len2 + 11
+    word_width = len("Слово:  ") + len(word)
+    index_width = len("Индекс: ") + len(index)
+    cases_width = len1 + len2 + 9
+    width = max(cases_width, word_width, index_width) + 2
+    len2 = max(len2, width - len1 - 9 - 2)
 
     print()
     print(f'╔{"═" * width}╗')
@@ -33,8 +37,8 @@ def run(word, index):
     print(f'╠═════╤═{"═" * len1}═╤═{"═" * len2}═╣')
     for key, key1, key2 in keys:
         print(f'║ {key} '
-              f'│ {replace_stress(result[key1]).ljust(len1)} '
-              f'│ {replace_stress(result[key2]).ljust(len2)} '
+              f'│ {replace_stress(result.get(key1, "-")).ljust(len1)} '
+              f'│ {replace_stress(result.get(key2, "-")).ljust(len2)} '
               f'║')
     print(f'╚═════╧═{"═" * len1}═╧═{"═" * len2}═╝')
     print()
