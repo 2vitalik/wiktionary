2b5ef16b326286c5f11dfe9685a93a7f1b623267
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-02-28 00:50:46 +0200

    parse/data/verb: Дополнительные данные по глаголу

--- libs/parse/data/blocks/detailed/verb.py
+++ libs/parse/data/blocks/detailed/verb.py
@@ -3,6 +3,15 @@ from libs.parse.utils.decorators import parsing
 
 
 class VerbData(BaseDetailedData):
+    tpls_no_index = [
+        'гл ru',
+        'гл ru СВ',
+        'гл ru НСВ',
+        'гл ru НВ',
+        'гл ru -ся',
+        'гл ru -сяСВ',
+        'гл ru -сяНСВ',
+    ]
 
     @classmethod
     def get_impersonal(cls, template):
@@ -18,16 +27,18 @@ class VerbData(BaseDetailedData):
         return False
 
     @classmethod
+    def get_aspect(cls, template):
+        if 'НСВ' in template.name:
+            return 'несов'
+        elif 'СВ' in template.name:
+            return 'сов'
+        else:
+            has_index = cls.check_index(template)
+            return "несов" if has_index else None
+
+    @classmethod
     def check_index(cls, template):
-        return template.name not in [
-            'гл ru',
-            'гл ru СВ',
-            'гл ru НСВ',
-            'гл ru НВ',
-            'гл ru -ся',
-            'гл ru -сяСВ',
-            'гл ru -сяНСВ',
-        ]
+        return template.name not in cls.tpls_no_index
 
     @classmethod
     def get_index(cls, template):
@@ -42,6 +53,10 @@ class VerbData(BaseDetailedData):
         indexes = []
         is_impersonal = None
         has_impersonal = None
+        has_perfective = None
+        has_imperfective = None
+        is_perfective_only = None
+        is_imperfective_only = None
         has_index = None
         for template in self.base.templates(re='гл*').last_list():
             index = self.get_index(template)  # todo: skip -ся, -сяСВ и т.п.
@@ -57,8 +72,28 @@ class VerbData(BaseDetailedData):
             else:
                 is_impersonal = False
 
+            aspect = self.get_aspect(template)
+            if aspect == 'сов':
+                has_perfective = True
+                is_imperfective_only = False
+                if is_perfective_only is None:
+                    is_perfective_only = True
+            elif aspect == 'несов':
+                has_imperfective = True
+                is_perfective_only = False
+                if is_imperfective_only is None:
+                    is_imperfective_only = True
+
             indexes.append(index)
+
         self._sub_data['indexes'] = indexes
         self._sub_data['has_index'] = has_index
         self._sub_data['is_impersonal'] = is_impersonal
         self._sub_data['has_impersonal'] = has_impersonal
+        self._sub_data['has_perfective'] = has_perfective
+        self._sub_data['has_imperfective'] = has_imperfective
+        self._sub_data['is_perfective_only'] = is_perfective_only
+        self._sub_data['is_imperfective_only'] = is_imperfective_only
+
+
+# todo: проверить, почему сломался "ять" на "check_index"
