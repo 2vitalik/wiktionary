697ba25147e18ba443ea95403d31a33c1af86d92
soshial <soshial@gmail.com>
2019-05-27 13:44:05 +0300

    fix typo; add comments

--- libs/parse/utils/decorators.py
+++ libs/parse/utils/decorators.py
@@ -1,4 +1,6 @@
 def parsed(func):
+    """This decorator makes sure, that before this func is called, the class is parsed.
+    If it is not parsed, parsing is lazily executed for this BaseSection"""
     def wrapped(self, *args, **kwargs):
         if self.is_parsing:
             raise Exception("Can't access an entry which is being parsing "
@@ -10,6 +12,7 @@ def parsed(func):
 
 
 def parsing(func):
+    """Decorator that marks parser code for this class. The func parses the class and its children (not recursively)"""
     def wrapped(self, *args, **kwargs):
         self.is_parsing = True
         result = func(self, *args, **kwargs)
