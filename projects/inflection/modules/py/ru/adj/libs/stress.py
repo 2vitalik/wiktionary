b6668ebbd944d7ed2c0c0c5cd823b092157fd7da
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-09-29 22:09:18 +0300

    inflection: Удаление неиспользуемого кода в прилагательных

--- projects/inflection/modules/py/ru/adj/libs/stress.py
+++ projects/inflection/modules/py/ru/adj/libs/stress.py
@@ -38,50 +38,6 @@ def extract_stress_type(rest_index):  # export
 
 
 # Данные: ударность основы и окончания в зависимости от схемы ударения
-def get_noun_stress_schema(stress_type):  # export  # INFO: Вычисление схемы ударения
-    _.log_func('stress', 'get_noun_stress_schema')
-
-    # local stress_schema, types, sg_value, pl_value
-
-    # общий подход следующий:
-    # если схема среди перечисленных, значит, элемент под ударением (stressed), иначе — нет (unstressed)
-    stress_schema = dict(  # dict
-        stem = dict(  # dict
-            sg     = _.equals(stress_type, ["a", "c", "e"]),
-            acc_sg = _.equals(stress_type, ["a", "c", "e", "d'", "f'"]),
-            ins_sg = _.equals(stress_type, ["a", "c", "e", "b'", "f''"]),
-            pl     = _.equals(stress_type, ["a", "d", "d'"]),
-            nom_pl = _.equals(stress_type, ["a", "d", "d'", "e", "f", "f'", "f''"]),
-        ),  # dict
-        ending = dict(  # dict
-            sg     = _.equals(stress_type, ["b", "b'", "d", "d'", "f", "f'", "f''"]),
-            acc_sg = _.equals(stress_type, ["b", "b'", "d", "f", "f''"]),
-            ins_sg = _.equals(stress_type, ["b", "d", "d'", "f", "f'"]),
-            pl     = _.equals(stress_type, ["b", "b'", "c", "e", "f", "f'", "f''"]),
-            nom_pl = _.equals(stress_type, ["b", "b'", "c"]),
-        ),  # dict
-    )  # dict
-
-    types = ['stem', 'ending']
-    for i, type in enumerate(types):
-        sg_value = stress_schema[type]['sg']
-        stress_schema[type]['nom_sg'] = sg_value
-        stress_schema[type]['gen_sg'] = sg_value
-        stress_schema[type]['dat_sg'] = sg_value
-        stress_schema[type]['prp_sg'] = sg_value
-
-        pl_value = stress_schema[type]['pl']
-        stress_schema[type]['gen_pl'] = pl_value
-        stress_schema[type]['dat_pl'] = pl_value
-        stress_schema[type]['ins_pl'] = pl_value
-        stress_schema[type]['prp_pl'] = pl_value
-    # end
-
-    return stress_schema
-# end
-
-
-# Данные: ударность основы и окончания в зависимости от схемы ударения
 def get_adj_stress_schema(stress_type):  # export  # INFO: Вычисление схемы ударения
     _.log_func('stress', 'get_adj_stress_schema')
 
@@ -118,28 +74,6 @@ def get_adj_stress_schema(stress_type):  # export  # INFO: Вычисление 
         # end
     # end
 
-    return stress_schema
-# end
-
-
-def get_pronoun_stress_schema(stress_type):  # export  # INFO: Вычисление схемы ударения
-    _.log_func('stress', 'get_pronoun_stress_schema')
-
-    # TODO: Пока не используется
-
-    # общий подход следующий:
-    # если схема среди перечисленных, значит, элемент под ударением (stressed), иначе — нет (unstressed)
-    # local stress_schema
-    stress_schema = dict(  # dict
-        stem = dict(  # dict
-            sg = _.equals(stress_type, "a"),
-            pl = _.equals(stress_type, "b"),
-        ),  # dict
-        ending = dict(  # dict
-            sg = _.equals(stress_type, "b"),
-            pl = _.equals(stress_type, "a"),
-        ),  # dict
-    )  # dict
     return stress_schema
 # end
 
