1225d4ce7a59e06de38f37e7e1b60d681022fca3
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-11-27 18:18:29 +0200

    web: Исправление для базовой папки проекта

--- web/project/urls.py
+++ web/project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from web.main.views import PageView, LogsFileView, LogsFolderView, TextView
+from main.views import PageView, LogsFileView, LogsFolderView, TextView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
