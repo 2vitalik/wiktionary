5eaf0884e810d7a45a752dfc2b50a4a31e829223
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-11-28 04:17:35 +0200

    recent: Disable `authors` storage temporarily (because of errors)

--- bin/recent.py
+++ bin/recent.py
@@ -7,6 +7,6 @@ from core.storage.updaters.shortcuts import update_recent_articles, \
 
 if __name__ == '__main__':
     update_recent_articles()
-    update_recent('authors')
+    # update_recent('authors')  # todo: process slack errors
     # todo: update_recent_sync()
     update_all_articles(MinuteStopper(59))
