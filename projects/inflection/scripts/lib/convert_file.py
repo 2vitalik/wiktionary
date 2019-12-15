8a7fd14d1d474dfca99ffd6ac96d21fcaed0347f
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-12-16 00:14:18 +0200

    inflection: Немножко переоформил пробелы и отступы

--- projects/inflection/scripts/lib/convert_file.py
+++ projects/inflection/scripts/lib/convert_file.py
@@ -25,60 +25,124 @@ string = '''('[^']*?'|"[^"]*?")'''
 
 regexps_MULTILINE = {
     'py': [
-        ('from projects.inflection.modules.{dev}.{dev}_py.a import syllables', 'local syllables = require("Модуль:слоги")'),
-        (r'from .libs import (\w+)', "local \\1 = require(parent_prefix .. '/\\1')"),
-        (r'from \.\.declension.sub import (\w+)', "local \\1 = require('Module:' .. dev_prefix .. 'inflection/ru/declension/\\1')"),
-        (r'from \.\.(\.)?(\w+) import (\w+) as (\w+)', "local \\4 = require('Module:' .. dev_prefix .. 'inflection/ru/\\2/\\3')  -- '\\1'"),
+        ('from projects.inflection.modules.{dev}.{dev}_py.a import syllables',
+         'local syllables = require("Модуль:слоги")'),
+
+        (r'from .libs import (\w+)',
+         "local \\1 = require(parent_prefix .. '/\\1')"),
+
+        (r'from \.\.declension.sub import (\w+)',
+         "local \\1 = require('Module:' .. dev_prefix .. 'inflection/ru/declension/\\1')"),
+
+        (r'from \.\.(\.)?(\w+) import (\w+) as (\w+)',
+         "local \\4 = require('Module:' .. dev_prefix .. 'inflection/ru/\\2/\\3')  -- '\\1'"),
+
         # (r"\bmw\.text\.", 'mw.'),
 
-        (r'stressed = 1', 'stressed = 2'),
-        (r'unstressed = 0', 'unstressed = 1'),
+        (r'stressed = 1',
+         r'stressed = 2'),
 
-        (r'^def (.*):  # export', 'function export.\\1'),
-        (r'^def (.*):', 'local function \\1'),
-        (r'# end\b', 'end'),
-        (r'# return export\n', 'return export\n'),
-        (r'^(\s+)([^=\n]+?) = (.*?)  # = export.\n', '\\1\\2 = export.\\3\n'),
-        (r'^(\s+)([^=\n]*?)  # export.\n', '\\1export.\\2\n'),
+        (r'unstressed = 0',
+         r'unstressed = 1'),
 
-        (r'^(\s*)([^=\n]+?) = (.*?)  # local\n', '\\1local \\2 = \\3\n'),
+        (r'^def (.*):  # export',
+         'function export.\\1'),
 
-        (r'\bpass\b', '-- pass'),
-        (r'# local ', 'local '),
-        (r'global ', '# global '),
-        (fr'''\[({string},\s*{string}(,\s*{string})*)\]''', '{\\1}'),
+        (r'^def (.*):',
+         'local function \\1'),
 
-        (r'\bif (.*?):', 'if \\1 then'),
-        (r'\belif (.*?):', 'elseif \\1 then'),
-        (r'\belse:', 'else'),
+        (r'# end\b',
+         'end'),
 
-        (r'\bstr\b', 'tostring'),
-        (r'\bTrue\b', 'true'),
-        (r'\bFalse\b', 'false'),
-        (r'\bNone\b', 'nil'),
+        (r'# return export\n',
+         'return export\n'),
 
-        (r'for (\w+) in range\((\w+), (\w+) \+ 1\):', 'for \\1 = \\2, \\3 do'),
-        (r'for (\w+), (\w+) in enumerate\((\w+)\):', 'for \\1, \\2 in pairs(\\3) do  -- list'),
-        (r'for (\w+), (\w+) in (.+).items\(\):', 'for \\1, \\2 in pairs(\\3) do'),
+        (r'^(\s+)([^=\n]+?) = (.*?)  # = export.\n',
+         '\\1\\2 = export.\\3\n'),
 
-        (r'a\.table_len\(', 'table.getn('),
-        (r'(\s*)(.*)\.append\((.*)\)', '\\1table.insert(\\2, \\3)'),
+        (r'^(\s+)([^=\n]*?)  # export.\n',
+         '\\1export.\\2\n'),
 
-        (r'_\.has_value\(([^)]+), ([^)]+)\)', '_.has_value(\\1[\\2])'),
-        (r'_\.has_key\(([^)]+), ([^)]+)\)', '_.has_key(\\1[\\2])'),
+        (r'^(\s*)([^=\n]+?) = (.*?)  # local\n',
+         '\\1local \\2 = \\3\n'),
 
-        (r''' \+ (?!['"]$|\d)''', ' .. '),
-        (r' != ', ' ~= '),
+        (r'\bpass\b',
+         '-- pass'),
 
-        (r"(\s)'(.*)': ", "\\1['\\2'] = "),
+        (r'# local ',
+         'local '),
 
-        (r'\[(1)\]', '[2]'),
-        (r'\[(0)\]', '[1]'),
+        (r'global ',
+         '# global '),
 
-        (r"(?<![&'])#(.*)$", '--\\1'),
-        (r"(?<![&'])#(.*)$", '--\\1'),  # Второй раз, чтобы обработать случаи `# .. # ..`
+        (fr'''\[({string},\s*{string}(,\s*{string})*)\]''',
+         '{\\1}'),
 
-        (r'^( +)-- INFO:', '--\\1INFO:'),
+        (r'\bif (.*?):',
+         'if \\1 then'),
+
+        (r'\belif (.*?):',
+         'elseif \\1 then'),
+
+        (r'\belse:',
+         'else'),
+
+        (r'\bstr\b',
+         'tostring'),
+
+        (r'\bTrue\b',
+         'true'),
+
+        (r'\bFalse\b',
+         'false'),
+
+        (r'\bNone\b',
+         'nil'),
+
+        (r'for (\w+) in range\((\w+), (\w+) \+ 1\):',
+         'for \\1 = \\2, \\3 do'),
+
+        (r'for (\w+), (\w+) in enumerate\((\w+)\):',
+         'for \\1, \\2 in pairs(\\3) do  -- list'),
+
+        (r'for (\w+), (\w+) in (.+).items\(\):',
+         'for \\1, \\2 in pairs(\\3) do'),
+
+        (r'a\.table_len\(',
+         'table.getn('),
+
+        (r'(\s*)(.*)\.append\((.*)\)',
+         '\\1table.insert(\\2, \\3)'),
+
+        (r'_\.has_value\(([^)]+), ([^)]+)\)',
+         '_.has_value(\\1[\\2])'),
+
+        (r'_\.has_key\(([^)]+), ([^)]+)\)',
+         '_.has_key(\\1[\\2])'),
+
+        (r''' \+ (?!['"]$|\d)''',
+         ' .. '),
+
+        (r' != ',
+         ' ~= '),
+
+        (r"(\s)'(.*)': ",
+         "\\1['\\2'] = "),
+
+        (r'\[(1)\]',
+         '[2]'),
+
+        (r'\[(0)\]',
+         '[1]'),
+
+        (r"(?<![&'])#(.*)$",
+         '--\\1'),
+
+        (r"(?<![&'])#(.*)$",
+         '--\\1'),  # Второй раз, чтобы обработать случаи `# .. # ..`
+
+        (r'^( +)-- INFO:',
+         '--\\1INFO:'),
 
         (r'^(--)?' + '    ' * 9, '\\1' + '\t' * 9),
         (r'^(--)?' + '    ' * 8, '\\1' + '\t' * 8),
@@ -91,84 +155,176 @@ regexps_MULTILINE = {
         (r'^(--)?' + '    ' * 1, '\\1' + '\t' * 1),
     ],
     'lua': [
-        ('local syllables = require\("Модуль:слоги"\)', 'from projects.inflection.modules.{dev}.{dev}_py.a import syllables'),
-        (r"^local (\w+) = require\(parent_prefix \.\. '/(\w+)'\)", 'from .libs import \\1'),
-        (r"local (\w+) = require\('Module:' \.\. dev_prefix \.\. 'inflection/ru/declension/(\w+)'\)", 'from ..declension.sub import \\1'),
-        (r"local (\w+) = require\('Module:' \.\. dev_prefix \.\. 'inflection/ru/(\w+)/(\w+)'\)  -- '(\.)?'", 'from ..\\4\\2 import \\3 as \\1'),
+        ('local syllables = require\("Модуль:слоги"\)',
+         'from projects.inflection.modules.{dev}.{dev}_py.a import syllables'),
+
+        (r"^local (\w+) = require\(parent_prefix \.\. '/(\w+)'\)",
+         'from .libs import \\1'),
+
+        (r"local (\w+) = require\('Module:' \.\. dev_prefix \.\. 'inflection/ru/declension/(\w+)'\)",
+         'from ..declension.sub import \\1'),
+
+        (r"local (\w+) = require\('Module:' \.\. dev_prefix \.\. 'inflection/ru/(\w+)/(\w+)'\)  -- '(\.)?'",
+         'from ..\\4\\2 import \\3 as \\1'),
+
         # (r"\bmw\.text\.", 'mw.'),
         # (r"\bmw\.ustring\.", 'mw.'),
 
-        (r'stressed = 2', 'stressed = 1'),
-        (r'unstressed = 1', 'unstressed = 0'),
+        (r'stressed = 2',
+         'stressed = 1'),
 
-        (r'^local function (.*?)(\s*--.*)?$', 'def \\1:\\2'),
-        (r'^function export\.(.*?)(\s*--.*)?$', 'def \\1:  # export\\2'),
-        (r'\bend\b', '# end'),
-        (r'return export\n', '# return export\n'),
-        (r'^(\s+)([^=\n]+?) = export\.(.*?)\n', '\\1\\2 = \\3  # = export.\n'),
-        (r'^(\s+)export\.(.*?)\n', '\\1\\2  # export.\n'),
+        (r'unstressed = 1',
+         'unstressed = 0'),
 
-        (r'^(\s*)local ([^=\n]+?) = (.*?)\n', '\\1\\2 = \\3  # local\n'),
+        (r'^local function (.*?)(\s*--.*)?$',
+         'def \\1:\\2'),
 
-        (r'-- pass\b', 'pass'),
-        (r'local ', '# local '),
-        (r'# global ', 'global '),
-        (r"\{('.*?', '.*?')\}", '[\\1]'),
-        (r'\{(".*?", ".*?")\}', '[\\1]'),
-        (r'\{(\'.*?\', ".*?")\}', '[\\1]'),
+        (r'^function export\.(.*?)(\s*--.*)?$',
+         'def \\1:  # export\\2'),
 
-        (r'\bif (.*) then', 'if \\1:'),
-        (r'\belseif (.*) then', 'elif \\1:'),
-        (r'\belse\b', 'else:'),
+        (r'\bend\b',
+         '# end'),
 
-        (r'\btostring\b', 'str'),
-        (r'\btrue\b', 'True'),
-        (r'\bfalse\b', 'False'),
-        (r'\bnil\b', 'None'),
+        (r'return export\n',
+         '# return export\n'),
 
-        (r'for (\w+) = (\w+), (\w+) do', 'for \\1 in range(\\2, \\3 + 1):'),
-        (r'for (\w+), (\w+) in pairs\((.*)\) do  -- list', 'for \\1, \\2 in enumerate(\\3):'),
-        (r'for (\w+), (\w+) in pairs\((.*)\) do', 'for \\1, \\2 in \\3.items():'),
+        (r'^(\s+)([^=\n]+?) = export\.(.*?)\n',
+         '\\1\\2 = \\3  # = export.\n'),
 
-        (r'table\.getn\(', 'a.table_len('),
-        (r'table\.insert\((.*), (.*)\)', '\\1.append(\\2)'),
+        (r'^(\s+)export\.(.*?)\n',
+         '\\1\\2  # export.\n'),
 
-        (r'_\.has_value\((\w+)\[([^]]+)\]\)', '_.has_value(\\1, \\2)'),
-        (r'_\.has_key\((\w+)\[([^]]+)\]\)', '_.has_key(\\1, \\2)'),
+        (r'^(\s*)local ([^=\n]+?) = (.*?)\n',
+         '\\1\\2 = \\3  # local\n'),
 
-        (r' \.\. ', ' + '),
-        (r' ~= ', ' != '),
+        (r'-- pass\b',
+         'pass'),
 
-        (r'\[(1)\]', '[0]'),
-        (r'\[(2)\]', '[1]'),
+        (r'local ',
+         '# local '),
 
-        (r"(\s)\['(.*)'\] = ", "\\1'\\2': "),
+        (r'# global ',
+         'global '),
 
-        (r'^--(\t+)INFO:', '\\1-- INFO:'),
+        (r"\{('.*?', '.*?')\}",
+         '[\\1]'),
 
-        (r'^--(.*)$', '#\\1'),
-        (r'(\s)--(.*)$', '\\1#\\2'),
+        (r'\{(".*?", ".*?")\}',
+         '[\\1]'),
+
+        (r'\{(\'.*?\', ".*?")\}',
+         '[\\1]'),
+
+        (r'\bif (.*) then',
+         'if \\1:'),
+
+        (r'\belseif (.*) then',
+         'elif \\1:'),
+
+        (r'\belse\b',
+         'else:'),
+
+        (r'\btostring\b',
+         'str'),
+
+        (r'\btrue\b',
+         'True'),
+
+        (r'\bfalse\b',
+         'False'),
+
+        (r'\bnil\b',
+         'None'),
+
+        (r'for (\w+) = (\w+), (\w+) do',
+         'for \\1 in range(\\2, \\3 + 1):'),
+
+        (r'for (\w+), (\w+) in pairs\((.*)\) do  -- list',
+         'for \\1, \\2 in enumerate(\\3):'),
+
+        (r'for (\w+), (\w+) in pairs\((.*)\) do',
+         'for \\1, \\2 in \\3.items():'),
+
+        (r'table\.getn\(',
+         'a.table_len('),
+
+        (r'table\.insert\((.*), (.*)\)',
+         '\\1.append(\\2)'),
+
+        (r'_\.has_value\((\w+)\[([^]]+)\]\)',
+         '_.has_value(\\1, \\2)'),
+
+        (r'_\.has_key\((\w+)\[([^]]+)\]\)',
+         '_.has_key(\\1, \\2)'),
+
+        (r' \.\. ',
+         ' + '),
+
+        (r' ~= ',
+         ' != '),
+
+        (r'\[(1)\]',
+         '[0]'),
+
+        (r'\[(2)\]',
+         '[1]'),
+
+        (r"(\s)\['(.*)'\] = ",
+         "\\1'\\2': "),
+
+        (r'^--(\t+)INFO:',
+         '\\1-- INFO:'),
+
+        (r'^--(.*)$',
+         '#\\1'),
+
+        (r'(\s)--(.*)$',
+         '\\1#\\2'),
 
         ('\t', r'    '),
     ],
 }
 regexps_DOTALL = {
     'py': [
-        (r'\[([^][]*)\]([,)]?)  (--|#) list',     '{\\1}\\2  -- list'),
-        (r'dict\(([^()]*)\)\)  (--|#) b-dict', '{\\1})  -- b-dict'),
-        (r'dict\(([^()]*)\)([,)]?)  (--|#) dict', '{\\1}\\2  -- dict'),
-        (r'a\.AttrDict\(([^()]*)\)([,)]?)  (--|#) AttrDict', '{\\1}\\2  -- AttrDict'),
-        (r'@a\.starts\(module\)\ndef ([\w]+)\(func(, )?([^\n]*?)\n', '# @starts\ndef \\1(\\3\n    func = "\\1"\n    _.starts(module, func)\n\n'),
-        (r'@a\.call\(module\)\ndef ([\w]+)\(([^\n]*?)\n', '# @call\ndef \\1(\\2\n    func = "\\1"\n    _.call(module, func)\n\n'),
+        (r'\[([^][]*)\]([,)]?)  (--|#) list',
+         '{\\1}\\2  -- list'),
+
+        (r'dict\(([^()]*)\)\)  (--|#) b-dict',
+         '{\\1})  -- b-dict'),
+
+        (r'dict\(([^()]*)\)([,)]?)  (--|#) dict',
+         '{\\1}\\2  -- dict'),
+
+        (r'a\.AttrDict\(([^()]*)\)([,)]?)  (--|#) AttrDict',
+         '{\\1}\\2  -- AttrDict'),
+
+        (r'@a\.starts\(module\)\ndef ([\w]+)\(func(, )?([^\n]*?)\n',
+         '# @starts\ndef \\1(\\3\n    func = "\\1"\n    _.starts(module, func)\n\n'),
+
+        (r'@a\.call\(module\)\ndef ([\w]+)\(([^\n]*?)\n',
+         '# @call\ndef \\1(\\2\n    func = "\\1"\n    _.call(module, func)\n\n'),
     ],
     'lua': [
-        (r'\{([^{}]*)\}([,)]?)  (--|#) list', '[\\1]\\2  # list'),
-        (r'\{([^{}]*)\}\)  (--|#) b-dict', 'dict(\\1))  # b-dict'),
-        (r'\{([^{}]*)\}([,)]?)  (--|#) dict', 'dict(\\1)\\2  # dict'),
-        (r'\{([^{}]*)\}([,)]?)  (--|#) AttrDict', 'a.AttrDict(\\1)\\2  # AttrDict'),
-        (r'(--|#) @starts\n(local )?function ([\w.]+)\(\)([^\n]*?)\n\tfunc = "[^"]+"\n\t_\.starts\(module, func\)\n\n', '@a.starts(module)\n\\2function \\3(func)\\4\n'),
-        (r'(--|#) @starts\n(local )?function ([\w.]+)\(([^\n]*?)\n\tfunc = "[^"]+"\n\t_\.starts\(module, func\)\n\n', '@a.starts(module)\n\\2function \\3(func, \\4\n'),
-        (r'(--|#) @call\n(local )?function ([\w.]+)\(([^\n]*?)\n\tfunc = "[^"]+"\n\t_\.call\(module, func\)\n\n', '@a.call(module)\n\\2function \\3(\\4\n'),
+        (r'\{([^{}]*)\}([,)]?)  (--|#) list',
+         '[\\1]\\2  # list'),
+
+        (r'\{([^{}]*)\}\)  (--|#) b-dict',
+         'dict(\\1))  # b-dict'),
+
+        (r'\{([^{}]*)\}([,)]?)  (--|#) dict',
+         'dict(\\1)\\2  # dict'),
+
+        (r'\{([^{}]*)\}([,)]?)  (--|#) AttrDict',
+         'a.AttrDict(\\1)\\2  # AttrDict'),
+
+        (r'(--|#) @starts\n(local )?function ([\w.]+)\(\)([^\n]*?)\n\tfunc = "[^"]+"\n\t_\.starts\(module, func\)\n\n',
+         '@a.starts(module)\n\\2function \\3(func)\\4\n'),
+
+        (r'(--|#) @starts\n(local )?function ([\w.]+)\(([^\n]*?)\n\tfunc = "[^"]+"\n\t_\.starts\(module, func\)\n\n',
+         '@a.starts(module)\n\\2function \\3(func, \\4\n'),
+
+        (r'(--|#) @call\n(local )?function ([\w.]+)\(([^\n]*?)\n\tfunc = "[^"]+"\n\t_\.call\(module, func\)\n\n',
+         '@a.call(module)\n\\2function \\3(\\4\n'),
     ],
 }
 regexps_before = {
