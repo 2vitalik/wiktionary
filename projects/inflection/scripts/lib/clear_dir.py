ed0396cc6e052c982fd50f4539b1ac30007c6c05
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-12-16 02:13:55 +0200

    inflection: При очистке out -- создавать папки, если нужно

--- projects/inflection/scripts/lib/clear_dir.py
+++ projects/inflection/scripts/lib/clear_dir.py
@@ -1,5 +1,6 @@
 from shutil import copy
 
+from libs.utils.io import ensure_parent_dir
 from projects.inflection.scripts.lib.compare_dir import compare_dir
 from projects.inflection.scripts.lib.files import files
 from projects.inflection.scripts.lib.paths import get_path
@@ -8,8 +9,9 @@ from projects.inflection.scripts.lib.paths import get_path
 def clear_out_dir(dev, lang):
     for file in files:
         # Копирование результата в `ru.out`:
-        copy(get_path(dev, lang, file, out=False),
-             get_path(dev, lang, file, out=True))
+        out_file = get_path(dev, lang, file, out=True)
+        ensure_parent_dir(out_file)
+        copy(get_path(dev, lang, file, out=False), out_file)
 
     if not compare_dir(dev, lang):
         print(f'Ошибка: папки `{lang}` не синхронизированы.')
