3135cba2d69b59410b1f73951dcda193001c5a86
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2020-01-01 18:48:42 +0200

    inflection: parse: Several renames, and split code into sub-functions

--- projects/inflection/modules/dev/dev_py/ru/declension/init/parse/common.py
+++ projects/inflection/modules/dev/dev_py/ru/declension/init/parse/common.py
@@ -14,8 +14,7 @@ module = 'init.parse.common'  # local
 
 
 @a.starts(module)
-def init_info(func, i):  # todo rename to `init_stem`
-    # local several_vowels, has_stress
+def init_stem(func, i):  # todo rename to `init_stem`
 
     # INFO: Исходное слово без ударения:
     i.word.unstressed = _.replaced(i.word.stressed, '́ ', '')  # todo: move outside this function
@@ -42,9 +41,9 @@ def init_info(func, i):  # todo rename to `init_stem`
     _.log_value(i.stem.unstressed, 'i.stem.unstressed')
     _.log_value(i.stem.stressed, 'i.stem.stressed')
 
-#  INFO: Случай, когда не указано ударение у слова:
-    several_vowels = _.contains_several(i.word.stressed, '{vowel+ё}')
-    has_stress = _.contains(i.word.stressed, '[́ ё]')
+    #  INFO: Случай, когда не указано ударение у слова:
+    several_vowels = _.contains_several(i.word.stressed, '{vowel+ё}')  # local
+    has_stress = _.contains(i.word.stressed, '[́ ё]')  # local
     if several_vowels and not has_stress:
         _.log_info('Ошибка: Не указано ударение в слове')
         e.add_error(i, 'Ошибка: Не указано ударение в слове')
@@ -57,24 +56,27 @@ def init_info(func, i):  # todo rename to `init_stem`
 
 @a.starts(module)
 def angle_brackets(func, i):
-    another_index = _.extract(i.rest_index, '%<([^>]+)%>')  # local
-    if another_index:
-        pt = i.pt  # local
-        if not pt:
+    angle_index = _.extract(i.rest_index, '%<([^>]+)%>')  # local
+    if angle_index:
+        if not i.pt:
             i.output_gender = i.gender
             i.output_animacy = i.animacy
         # end
+
         i.orig_index = i.index
-        i.index = another_index
+        i.index = angle_index
+
+        pt_backup = i.pt  # local
         noun_parse.extract_gender_animacy(i)
-        i.pt = pt
+        i.pt = pt_backup
+
         if e.has_error(i):
             return _.ends(module, func)
         # end
 
         _.log_value(i.adj, 'i.adj')
         if i.adj:  # fixme: Для прилагательных надо по-особенному?
-            init_info(i)
+            init_stem(i)
             if e.has_error(i):
                 return _.ends(module, func)
             # end
@@ -88,8 +90,12 @@ def angle_brackets(func, i):
 @a.starts(module)
 def parse(func, base, args):  # export
     i = a.AttrDict()  # AttrDict  # local
-    i.word = a.AttrDict()  # AttrDict                                      #
-    i.stem = a.AttrDict()  # AttrDict                                      #
+    i.word = a.AttrDict()  # AttrDict
+    i.stem = a.AttrDict()  # AttrDict
+    i.parts = a.AttrDict()  # AttrDict
+    i.result = a.AttrDict()  # AttrDict
+    i.result.error = ''
+    i.has_index = True  # изначально предполагаем, что индекс есть
 
     # INFO: Достаём значения из параметров:
     i.base = base
@@ -99,12 +105,6 @@ def parse(func, base, args):  # export
     i.index = mw.text.trim(args['индекс'])
     i.word.stressed = mw.text.trim(args['слово'])
     i.noun = (i.unit == 'noun')
-
-    i.parts = a.AttrDict()  # AttrDict
-    i.result = a.AttrDict()  # AttrDict
-    i.result.error = ''
-
-    i.has_index = True  # изначально предполагаем, что индекс есть
 
     _.log_value(i.index, 'i.index')
     _.log_value(i.word.stressed, 'i.word.stressed')
@@ -116,7 +116,7 @@ def parse(func, base, args):  # export
 
     _.log_info('Получение информации о роде и одушевлённости')
 
-    if i.noun:  # fxime
+    if i.noun:  # fixme
         noun_parse.extract_gender_animacy(i)
         if e.has_error(i):
             _.ends(module, func)
@@ -139,7 +139,7 @@ def parse(func, base, args):  # export
     _.log_value(i.rest_index, 'i.rest_index')
 
     # INFO: stem, stem.stressed, etc.
-    init_info(i)  # todo: rename to `init_stem`
+    init_stem(i)
     if e.has_error(i):
         _.ends(module, func)
         return i
@@ -155,22 +155,12 @@ def parse(func, base, args):  # export
     # end
 
     # INFO: Проверяем случай с вариациями:
-    parts = mw.text.split(i.rest_index, '//')  # local
-    n_parts = a.table_len(parts)  # local
+    variations = mw.text.split(i.rest_index, '//')  # local  # todo: rename
+    n_variations = a.table_len(variations)  # local
 
-    if n_parts == 1:  # INFO: Дополнительных вариаций нет
+    if n_variations == 1:  # INFO: Дополнительных вариаций нет
         if _.contains(i.animacy, '//'):  # INFO: Случаи 'in//an' и 'an//in'
-            # INFO: Клонируем две вариации на основании текущих данных
-            i_1 = mw.clone(i)  # local
-            i_2 = mw.clone(i)  # local
-
-            # INFO: Устанавливаем для них соответствующую вариацию одушевлённости
-            i_1.animacy = mw.ustring.sub(i.animacy, 1, 2)
-            i_2.animacy = mw.ustring.sub(i.animacy, 5, 6)
-
-            # INFO: Заполняем атрибут с вариациями
-            i.variations = [p.process(i_1), p.process(i_2)]  # list
-
+            process_animacy_variations(i)
             _.ends(module, func)
             return i
             # TODO: А что если in//an одновременно со следующими случаями "[]" или "+"
@@ -178,35 +168,11 @@ def parse(func, base, args):  # export
 
         # _.log_info('Случай с "+" (несколько составных частей слова через дефис)')
 
-        index_parts = mw.text.split(i.rest_index, '%+')  # local
-        words_parts = mw.text.split(i.word.stressed, '-')  # local
-        n_sub_parts = a.table_len(index_parts)  # local
-        if n_sub_parts > 1:
-            i.plus = []  # list
-            for j in range(n_sub_parts):
-                i_copy = mw.clone(i)  # local
-                i_copy.word.stressed = words_parts[i]
-
-                init_info(i_copy)
-                if e.has_error(i_copy):
-                    e.add_error(i, i_copy.result.error)
-                    _.ends(module, func)
-                    return i
-                # end
-
-                i_copy.rest_index = index_parts[i]
-
-                if i.noun:
-                    angle_brackets(i_copy)
-                    if e.has_error(i_copy):
-                        e.add_error(i, i_copy.result.error)
-                        _.ends(module, func)
-                        return i
-                    # end
-                # end
-
-                i.plus.append(p.process(i_copy))
-            # end
+        plus_index = mw.text.split(i.rest_index, '%+')  # local
+        plus_words = mw.text.split(i.word.stressed, '-')  # local
+        n_plus = a.table_len(plus_index)  # local
+        if n_plus > 1:
+            process_plus(i, plus_words, plus_index)
             _.ends(module, func)
             return i
         # end
@@ -220,27 +186,12 @@ def parse(func, base, args):  # export
         # end
 
         if _.contains(i.rest_index, '%[%([12]%)%]') or _.contains(i.rest_index, '%[[①②]%]'):
-            # INFO: Клонируем две вариации на основании текущих данных
-            i_1 = mw.clone(i)  # local
-            i_2 = mw.clone(i)  # local
-
-            # INFO: Устанавливаем факультативность (первый случай):
-            i_1.rest_index = _.replaced(i_1.rest_index, '%[(%([12]%))%]', '')
-            i_1.rest_index = _.replaced(i_1.rest_index, '%[([①②])%]', '')
-
-            # INFO: Устанавливаем факультативность (второй случай):
-            i_2.rest_index = _.replaced(i_2.rest_index, '%[(%([12]%))%]', '%1')
-            i_2.rest_index = _.replaced(i_2.rest_index, '%[([①②])%]', '%1')
-            i_2.rest_index = _.replaced(i_2.rest_index, '%*', '')
-
-            # INFO: Заполняем атрибут с вариациями
-            i.variations = [p.process(i_1), p.process(i_2)]  # list
-
+            process_brackets_variations(i)
             _.ends(module, func)
             return i
         # end
 
-    elif n_parts == 2:  # INFO: Вариации "//" для ударения (и прочего индекса)
+    elif n_variations == 2:  # INFO: Вариации "//" для ударения (и прочего индекса)
         _.log_info('> Случай с вариациями //')
 
         if _.contains(i.animacy, '//'):
@@ -250,41 +201,7 @@ def parse(func, base, args):  # export
             return i
         # end
 
-        # INFO: Клонируем две вариации на основании текущих данных
-        i_1 = mw.clone(i)  # local
-        i_2 = mw.clone(i)  # local
-
-        # INFO: Предпогалаем, что у нас пока не "полная" вариация (не затрагивающая род)
-        i_1.rest_index = parts[0]
-        i_2.rest_index = parts[1]
-
-        if i.noun:
-            # INFO: Проверяем, не находится ли род+одушевлённость во второй вариации
-            i_2.index = parts[1]  # INFO: Для этого инициируем `.index`, чтобы его обработала функция `extract_gender_animacy`
-            noun_parse.extract_gender_animacy(i_2)
-        # end
-
-        # INFO: Если рода и одушевлённости во второй вариации нет (простой случай):
-        if not i_2.gender and not i_2.animacy:
-            # INFO: Восстанавливаем прежние общие значения:
-            i_2.gender = i.gender
-            i_2.animacy = i.animacy
-            i_2.common_gender = i.common_gender
-
-        # INFO: Проверка на гипотетическую ошибку в алгоритме:
-        elif not i_2.gender and i_2.animacy or i_2.gender and not i_2.animacy:
-            e.add_error(i, 'Странная ошибка: После `extract_gender_animacy` не может быть частичной заполненности полей')
-            _.ends(module, func)
-            return i
-
-        # INFO: Если что-то изменилось, значит, прошёл один из случаев, и значит у нас "полная" вариация (затрагивающая род)
-        elif i.gender != i_2.gender or i.animacy != i_2.animacy or i.common_gender != i_2.common_gender:
-            i.rest_index = None  # INFO: Для случая "полной" вариации понятие `rest_index`, наверное, не определено
-        # end
-        i_2.index = i.index  # INFO: Возвращаем исходное значение `index`; инвариант: оно всегда будет равно исходному индексу
-
-        # INFO: Заполняем атрибут с вариациями
-        i.variations = [p.process(i_1), p.process(i_2)]  # list
+        process_full_variations(i, variations)
 
         _.ends(module, func)
         return i
@@ -297,6 +214,120 @@ def parse(func, base, args):  # export
 
     _.ends(module, func)
     return p.process(i)
+# end
+
+
+@a.starts(module)
+def process_animacy_variations(func, i):
+    # INFO: Клонируем две вариации на основании текущих данных
+    i_1 = mw.clone(i)  # local
+    i_2 = mw.clone(i)  # local
+
+    # INFO: Устанавливаем для них соответствующую вариацию одушевлённости
+    i_1.animacy = mw.ustring.sub(i.animacy, 1, 2)
+    i_2.animacy = mw.ustring.sub(i.animacy, 5, 6)
+
+    # INFO: Заполняем атрибут с вариациями
+    i.variations = [p.process(i_1), p.process(i_2)]  # list
+
+    _.ends(module, func)
+# end
+
+
+@a.starts(module)
+def process_plus(func, i, plus_words, plus_index):
+    i.plus = []  # list
+    n_plus = a.table_len(plus_index)  # local
+    for j in range(n_plus):
+        i_copy = mw.clone(i)  # local
+        i_copy.word.stressed = plus_words[i]
+
+        init_stem(i_copy)
+        if e.has_error(i_copy):
+            e.add_error(i, i_copy.result.error)
+            _.ends(module, func)
+            return
+        # end
+
+        i_copy.rest_index = plus_index[j]
+
+        if i.noun:
+            angle_brackets(i_copy)
+            if e.has_error(i_copy):
+                e.add_error(i, i_copy.result.error)
+                _.ends(module, func)
+                return
+            # end
+        # end
+
+        i.plus.append(p.process(i_copy))
+    # end
+
+    _.ends(module, func)
+# end
+
+
+@a.starts(module)
+def process_brackets_variations(func, i):
+    # INFO: Клонируем две вариации на основании текущих данных
+    i_1 = mw.clone(i)  # local
+    i_2 = mw.clone(i)  # local
+
+    # INFO: Устанавливаем факультативность (первый случай):
+    i_1.rest_index = _.replaced(i_1.rest_index, '%[(%([12]%))%]', '')
+    i_1.rest_index = _.replaced(i_1.rest_index, '%[([①②])%]', '')
+
+    # INFO: Устанавливаем факультативность (второй случай):
+    i_2.rest_index = _.replaced(i_2.rest_index, '%[(%([12]%))%]', '%1')
+    i_2.rest_index = _.replaced(i_2.rest_index, '%[([①②])%]', '%1')
+    i_2.rest_index = _.replaced(i_2.rest_index, '%*', '')
+
+    # INFO: Заполняем атрибут с вариациями
+    i.variations = [p.process(i_1), p.process(i_2)]  # list
+
+    _.ends(module, func)
+# end
+
+
+@a.starts(module)
+def process_full_variations(func, i, parts):
+    # INFO: Клонируем две вариации на основании текущих данных
+    i_1 = mw.clone(i)  # local
+    i_2 = mw.clone(i)  # local
+
+    # INFO: Предпогалаем, что у нас пока не "полная" вариация (не затрагивающая род)
+    i_1.rest_index = parts[0]
+    i_2.rest_index = parts[1]
+
+    if i.noun:
+        # INFO: Проверяем, не находится ли род+одушевлённость во второй вариации
+        i_2.index = parts[1]  # INFO: Для этого инициируем `.index`, чтобы его обработала функция `extract_gender_animacy`
+        noun_parse.extract_gender_animacy(i_2)
+    # end
+
+    # INFO: Если рода и одушевлённости во второй вариации нет (простой случай):
+    if not i_2.gender and not i_2.animacy:
+        # INFO: Восстанавливаем прежние общие значения:
+        i_2.gender = i.gender
+        i_2.animacy = i.animacy
+        i_2.common_gender = i.common_gender
+
+    # INFO: Проверка на гипотетическую ошибку в алгоритме:
+    elif not i_2.gender and i_2.animacy or i_2.gender and not i_2.animacy:
+        e.add_error(i, 'Странная ошибка: После `extract_gender_animacy` не может быть частичной заполненности полей')
+        _.ends(module, func)
+        return
+
+    # INFO: Если что-то изменилось, значит, прошёл один из случаев, и значит у нас "полная" вариация (затрагивающая род)
+    elif i.gender != i_2.gender or i.animacy != i_2.animacy or i.common_gender != i_2.common_gender:
+        i.rest_index = None  # INFO: Для случая "полной" вариации понятие `rest_index`, наверное, не определено
+    # end
+    i_2.index = i.index  # INFO: Возвращаем исходное значение `index`; инвариант: оно всегда будет равно исходному индексу
+
+    # INFO: Заполняем атрибут с вариациями
+    i.variations = [p.process(i_1), p.process(i_2)]  # list
+
+    _.ends(module, func)
 # end
 
 
