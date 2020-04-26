ad04706dc7eecb803a78c111b4ec72eb77ad5ae2
Vitaliy Lyapota <2vitalik@gmail.com>
2020-04-26 21:07:29 +0300

    data: Remove debugging from `update_langs` cron job

--- projects/parsed/update_lists/update_langs.py
+++ projects/parsed/update_lists/update_langs.py
@@ -8,15 +8,9 @@ from libs.utils.io import write
 
 def update_langs():
     lists = defaultdict(list)
-    i = 0
     for title, page in storage.iterate_pages(silent=True):
-        print(i, title)
         for lang in page.languages.keys:
-            print(lang)
             lists[lang].append(title)
-        if i > 3000:
-            break
-        i += 1
     path = join(conf.PARSED_STORAGE_PATH, 'lists', 'langs')
     for lang, titles in lists.items():
         write(f'{path}/{lang or "-"}.txt', '\n'.join(titles))
