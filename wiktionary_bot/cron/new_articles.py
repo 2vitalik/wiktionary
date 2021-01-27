4b0ff5c80bde32ff0e81ae87cee8d5f4ea6358d4
Vitaliy Lyapota <2vitalik@gmail.com>
2021-01-27 03:02:57 +0200

    new-articles: Update messages in channel if they changed in Wiktionary

--- wiktionary_bot/cron/new_articles.py
+++ wiktionary_bot/cron/new_articles.py
@@ -8,12 +8,12 @@ from pywikibot.pagegenerators import RecentChangesPageGenerator
 
 from core.conf import conf
 from libs.parse.sections.page import Page
-from libs.utils.io import write, read, read_lines, append
+from libs.utils.io import write, read, read_lines, append, json_load, json_dump
 from libs.utils.lock import locked_repeat
 from libs.utils.wikibot import Namespace
 from wiktionary_bot.cron.bots import bots
 from wiktionary_bot.src.semantic import Reply, get_author
-from wiktionary_bot.src.utils import send
+from wiktionary_bot.src.utils import send, edit
 
 
 def convert_date(value):
@@ -48,9 +48,26 @@ class titles:
         append(cls.filename, title)
 
 
+class messages:
+    filename = join(conf.data_path, 'new_articles', 'messages.json')
+    ids = json_load(filename)
+
+    @classmethod
+    def get(cls, title):
+        return cls.ids[title]
+
+    @classmethod
+    def set(cls, title, message_id):
+        if title in cls.ids:
+            pass  # todo: log to slack: message was recreated?
+        cls.ids[title] = message_id
+        json_dump(cls.filename, cls.ids)
+
+
 def get_new_articles():
     old_titles = titles.get()
     new_titles = []
+    changed_titles = set()
     generator = RecentChangesPageGenerator(
         # start=datetime(2021, 1, 25, 16, 50),  # info: just for debug
         end=latest_date.get(),
@@ -71,7 +88,11 @@ def get_new_articles():
         latest_edited = latest_edited or convert_date(edited)
         if title in new_titles:
             continue
+        if title in changed_titles:
+            continue
         if title in old_titles:
+            if title in messages.ids:
+                changed_titles.add(title)
             continue
         has_offensive = False
         offensives = ['{{off}}', '{{offensive}}', '{{Offensive}}']
@@ -84,20 +105,28 @@ def get_new_articles():
             new_titles.append(title)
     if latest_edited:
         latest_date.set(latest_edited)
-    return new_titles
+    return new_titles, changed_titles
 
 
 @locked_repeat('new_articles')
 def process_new_articles():
     bot = telegram.Bot(conf.telegram_token)
-    new_titles = get_new_articles()
+    new_titles, changed_titles = get_new_articles()
     chat_id = conf.new_channel_id
     for title in reversed(new_titles):
         reply = Reply(title)
         text = reply.text + get_author(title)
         if '🔻 Секция «Семантические свойства» не найдена' not in reply.text:
-            send(bot, chat_id, text, reply_markup=reply.buttons)
+            msg = send(bot, chat_id, text)
+            messages.set(title, msg.message_id)
         titles.add(title)
+    for title in changed_titles:
+        message_id = messages.ids[title]
+        reply = Reply(title)
+        text = reply.text + get_author(title)
+        if '🔻 Секция «Семантические свойства» не найдена' not in reply.text:
+            edit(bot, chat_id, message_id, text)
+            # todo: send report to slack!
 
 
 if __name__ == '__main__':
