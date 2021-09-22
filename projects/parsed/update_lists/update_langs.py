2fd56a86b2d0aff252543270a468ca0275e8293c
2vitalik <2vitalik@gmail.com>
2021-09-22 23:04:45 +0300

    langs: Add lang-list files without redirects

--- projects/parsed/update_lists/update_langs.py
+++ projects/parsed/update_lists/update_langs.py
@@ -7,12 +7,20 @@ from libs.utils.io import write
 
 
 def update_langs():
-    lists = defaultdict(list)
+    all_articles = defaultdict(list)
+    without_redirects = defaultdict(list)
     for title, page in storage.iterate_pages(silent=True):
         for lang in page.languages.keys:
-            lists[lang].append(title)
+            all_articles[lang].append(title)
+            if not page.is_redirect:
+                without_redirects[lang].append(title)
+
     path = join(conf.PARSED_STORAGE_PATH, 'lists', 'langs')
-    for lang, titles in lists.items():
+    for lang, titles in all_articles.items():
+        write(f'{path}/{lang or "-"}.txt', '\n'.join(titles))
+
+    path = join(conf.PARSED_STORAGE_PATH, 'lists', 'langs', 'articles')
+    for lang, titles in without_redirects.items():
         write(f'{path}/{lang or "-"}.txt', '\n'.join(titles))
 
 
