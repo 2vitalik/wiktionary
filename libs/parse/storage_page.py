5204a9e68128959aa23ca8403bba989c582aa621
2vitalik <2vitalik@gmail.com>
2021-09-22 23:26:08 +0300

    libs: Add `IteratedStoragePage` class

--- libs/parse/storage_page.py
+++ libs/parse/storage_page.py
@@ -2,12 +2,17 @@ from core.storage.main import MainStorage
 from libs.parse.sections.page import Page
 
 
-class StoragePage(Page):
+class IteratedStoragePage(Page):
+    def __init__(self, title, content, info, silent=False):
+        is_redirect = info.endswith('R')
+        super().__init__(title, content, is_redirect=is_redirect,
+                         silent=silent)
+
+
+class StoragePage(IteratedStoragePage):  # todo: rename to `SingleStoragePage`
     storage = MainStorage()
 
     def __init__(self, title, silent=False, skip_absent=False):
         content = self.storage.get(title, 'content', silent=skip_absent)
         info = self.storage.get(title, 'info', silent=skip_absent)
-        is_redirect = info.endswith('R')
-        super().__init__(title, content, is_redirect=is_redirect,
-                         silent=silent)
+        super().__init__(title, content, info, silent=silent)
