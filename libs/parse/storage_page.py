28155a08980d532348f367ff3a648595076e952f
2vitalik <2vitalik@gmail.com>
2022-01-29 03:32:04 +0200

    parse: Add couple descriptions

--- libs/parse/storage_page.py
+++ libs/parse/storage_page.py
@@ -4,6 +4,9 @@ from libs.parse.sections.page import Page
 
 class IteratedStoragePage(Page):
     def __init__(self, title, content, info, silent=False):
+        """
+        :param silent: Don't fail on strict parsing rules
+        """
         is_redirect = info.endswith('R')
 
         super().__init__(title, content, is_redirect=is_redirect,
@@ -14,6 +17,10 @@ class StoragePage(IteratedStoragePage):  # todo: rename to `SingleStoragePage`
     storage = MainStorage()
 
     def __init__(self, title, silent=False, skip_absent=False):
+        """
+        :param silent: Don't fail on strict parsing rules
+        :param skip_absent: Return '' if page is absent in storage
+        """
         content = self.storage.get(title, 'content', silent=skip_absent)
         info = self.storage.get(title, 'info', silent=skip_absent)
 
