e1a07ded7bcb2d2096d8f612f67388f45dd5cbbd
2vitalik <2vitalik@gmail.com>
2022-01-30 22:17:15 +0200

    data: Implement page data class

--- libs/parse/data/page.py
+++ libs/parse/data/page.py
@@ -5,8 +5,57 @@ from libs.parse.utils.decorators import parsing
 
 class PageData(BaseData):
 
+    @property
+    def languages(self):
+        return self.sub_data.items()
+
     @parsing
     def _parse(self):
         self._sub_data = {}
         for lang, lang_section in self.base.sub_sections.items():
-            self._sub_data[lang] = LanguageData(lang_section)
+            self._sub_data[lang] = \
+                LanguageData(lang_section, None, self.page, lang)
+
+    @property
+    def parsed_data(self):
+        data = {}
+
+        warnings = {
+            # https://ru.wiktionary.org/wiki/Категория:Викисловарь:Шаблоны:Быстрое_удаление
+            'wikify': [
+                '{{wikify',
+            ],
+            'editing': [
+                '{{пишу',
+            ],
+            'to-delete': [
+                '{{к удалению', '{{db-', '{{уд-', '{{NCT', '{{hangon',
+                '{{orphaned-fairuse',
+            ],
+        }
+        data['base'] = {'warnings': []}
+        for warning, templates in warnings.items():
+            for template in templates:
+                if template in self.page.content:
+                    data['base']['warnings'].append(warning)
+
+        if self.page.is_redirect:
+            data['base']['redirect'] = True
+            return data
+
+        if ' ' in self.page.title:
+            data['base']['type'] = 'phrase'
+            return data
+
+        if self.page.title.startswith('*'):
+            data['base']['type'] = 'proto-slavic'
+            return data
+
+        if self.page.title.startswith('-') or self.page.title.endswith('-'):
+            data['base']['type'] = 'morpheme'
+            return data
+
+        for lang, language in self.page.data.languages:
+            data[lang] = language.parsed_data
+
+        return data
