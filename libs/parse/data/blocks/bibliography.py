7cd4f2e62b1c82ce89fe24b28d1546fd5bfdd69c
2vitalik <2vitalik@gmail.com>
2022-01-30 22:09:45 +0200

    data: Add several empty blocks data (implement later)

--- libs/parse/data/blocks/bibliography.py
+++ libs/parse/data/blocks/bibliography.py
@@ -2,7 +2,7 @@ from libs.parse.data.blocks.base import BaseBlockData
 from libs.parse.utils.decorators import parsing
 
 
-class PhrasesData(BaseBlockData):
+class BibliographyData(BaseBlockData):
     @parsing
     def _parse(self):
         self._sub_data = {}
--- libs/parse/data/blocks/related_words.py
+++ libs/parse/data/blocks/related_words.py
@@ -1,5 +1,8 @@
 from libs.parse.data.blocks.base import BaseBlockData
+from libs.parse.utils.decorators import parsing
 
 
 class RelatedWordsData(BaseBlockData):
-    pass
+    @parsing
+    def _parse(self):
+        self._sub_data = {}
