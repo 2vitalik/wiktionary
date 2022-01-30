a6c379891333915c8d64fa053362fb37b799c2f0
2vitalik <2vitalik@gmail.com>
2022-01-30 22:14:16 +0200

    data: Implement semantic block

--- libs/parse/data/blocks/semantic.py
+++ libs/parse/data/blocks/semantic.py
@@ -1,5 +1,36 @@
 from libs.parse.data.blocks.base import BaseBlockData
+from libs.parse.data.blocks.chooser import BlockData
+from libs.parse.data.blocks.detailed.semantic.top import SemanticTopData
+from libs.parse.utils.decorators import parsing
 
 
 class SemanticData(BaseBlockData):
-    pass
+    @parsing
+    def _parse(self):
+        self._sub_data = {
+            'top': SemanticTopData(self.base, self, self.page),
+        }
+        for block_key, block_section in self.base.sub_sections.items():
+            block_data_class = BlockData.get_class(block_key)
+            self._sub_data[block_key] = \
+                block_data_class(block_section, self, self.page)
+
+    @property
+    def parsed_data(self):
+        if self.base.templates('значение'):
+            return {'todo'}  # fixme
+        if self.base.templates('семантика'):
+            return {'todo'}  # fixme
+
+        data = {
+            'top': self['top'].parsed_data,
+        }
+        headers = [
+            'definition', 'synonyms', 'antonyms', 'hyperonyms', 'hyponyms',
+        ]
+        for header in headers:
+            if header not in self.sub_data:
+                data[header] = {'header': False}
+            else:
+                data[header] = self[header].parsed_data
+        return data
