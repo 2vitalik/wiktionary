b4e4697ac44e5097ff31edb95e8a64240b67a0a3
2vitalik <2vitalik@gmail.com>
2022-01-30 22:15:03 +0200

    data: Improve chooser block data class

--- libs/parse/data/blocks/chooser.py
+++ libs/parse/data/blocks/chooser.py
@@ -1,31 +1,42 @@
 from libs.parse.data.blocks.base import BaseBlockData
-from libs.parse.data.blocks.etymology import EtymologyData
-from libs.parse.data.blocks.morphology import MorphologyData
-from libs.parse.data.blocks.phrases import PhrasesData
-from libs.parse.data.blocks.pronunciation import PronunciationData
-from libs.parse.data.blocks.related_words import RelatedWordsData
-from libs.parse.data.blocks.semantic import SemanticData
-from libs.parse.data.blocks.translate import TranslateData
 
 
 class BlockData:
-    classes = {
-        'morphology': MorphologyData,
-        'pronunciation': PronunciationData,
-        'semantic': SemanticData,
-        # 'definition': DefinitionData,
-        # 'synonyms': SynonymsData,
-        # 'antonyms': AntonymsData,
-        # 'hyperonyms': HyperonymsData,
-        # 'hyponyms': HyponymsData,
-        'related_words': RelatedWordsData,
-        'etymology': EtymologyData,
-        'phrases': PhrasesData,
-        'translate': TranslateData,
-        # 'bibliography': BibliographyData,
-
-    }
-
     @classmethod
     def get_class(cls, block_key):
-        return cls.classes.get(block_key, BaseBlockData)
+        from libs.parse.data.blocks.bibliography import BibliographyData
+        from libs.parse.data.blocks.detailed.semantic.definition import \
+            DefinitionData
+        from libs.parse.data.blocks.detailed.semantic.onyms.antonyms import \
+            AntonymsData
+        from libs.parse.data.blocks.detailed.semantic.onyms.hyperonyms import \
+            HyperonymsData
+        from libs.parse.data.blocks.detailed.semantic.onyms.hyponyms import \
+            HyponymsData
+        from libs.parse.data.blocks.detailed.semantic.onyms.synonyms import \
+            SynonymsData
+        from libs.parse.data.blocks.etymology import EtymologyData
+        from libs.parse.data.blocks.morphology import MorphologyData
+        from libs.parse.data.blocks.phrases import PhrasesData
+        from libs.parse.data.blocks.pronunciation import PronunciationData
+        from libs.parse.data.blocks.related_words import RelatedWordsData
+        from libs.parse.data.blocks.semantic import SemanticData
+        from libs.parse.data.blocks.translation import TranslationData
+
+        classes = {
+            'morphology': MorphologyData,
+            'pronunciation': PronunciationData,
+            'semantic': SemanticData,
+            'definition': DefinitionData,
+            'synonyms': SynonymsData,
+            'antonyms': AntonymsData,
+            'hyperonyms': HyperonymsData,
+            'hyponyms': HyponymsData,
+            'related_words': RelatedWordsData,
+            'etymology': EtymologyData,
+            'phrases': PhrasesData,
+            'translation': TranslationData,
+            'bibliography': BibliographyData,
+        }
+
+        return classes.get(block_key, BaseBlockData)
