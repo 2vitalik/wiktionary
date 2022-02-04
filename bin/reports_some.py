9e13faff7ba9178ad3618016ea8081bd786d906d
2vitalik <2vitalik@gmail.com>
2022-02-04 23:12:04 +0200

    report: Change report in "some" command

--- bin/reports_some.py
+++ bin/reports_some.py
@@ -1,8 +1,7 @@
 import up  # don't remove this
-from core.reports.reports.ru.verbs.without_transcription import \
-    VerbsWithoutTranscription
+from core.reports.reports.templates.tpl_length import WrongLength
 from core.reports.scripts.run import reports_some
 
 
 if __name__ == '__main__':
-    reports_some([VerbsWithoutTranscription])
+    reports_some([WrongLength])
