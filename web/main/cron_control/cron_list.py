d83537348eb9521f133760d9fd0370fc7012d4fe
Vitalik <2vitalik@gmail.com>
2025-11-27 19:56:25 +0200

    web/cron: Prettify only titles on the page (leave slug as is)

--- web/main/cron_control/cron_list.py
+++ web/main/cron_control/cron_list.py
@@ -18,7 +18,8 @@ def get_cron_list():
                 raise
             cron_list.append({
                 'prj': prj,
-                'slug': slug.replace('_', ' ').title(),
+                'slug': slug,
+                'title': slug.replace('_', ' ').title(),
                 'status': status,
                 'modified': modified,
                 'delta_str': delta_str,
