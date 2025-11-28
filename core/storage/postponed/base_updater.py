d9505a0f74d06d4584e1b7cd10c92bdd3f40f77f
Vitalik <2vitalik@gmail.com>
2025-11-28 12:18:14 +0200

    tg/errors: Remove duplicated thing (exceptions are already sending)

--- core/storage/postponed/base_updater.py
+++ core/storage/postponed/base_updater.py
@@ -1,15 +1,11 @@
 from genericpath import exists
 from os.path import join
 
-import telegram
-
-from core.conf import conf
 from core.storage.main import storage
 from core.storage.postponed.debug.debug import DebugMixin
 from libs.utils.dt import dtp, dt
 from libs.utils.io import read, write
 from wiktionary_bot.src.slack import slack_error
-from wiktionary_bot.src.utils import send
 
 
 class PostponedUpdaterMixin(DebugMixin):
@@ -44,10 +40,8 @@ class PostponedUpdaterMixin(DebugMixin):
             return None
         content = read(self.latest_updated_filename)
         if not content:
-            msg = f'⛔ Empty file: {self.latest_updated_filename}'
+            msg = f'Empty file: {self.latest_updated_filename}'
             slack_error(msg)
-            bot = telegram.Bot(conf.telegram_token)
-            send(bot, conf.main_group_id, msg)
             raise ValueError(msg)
         return dtp(content)
 
