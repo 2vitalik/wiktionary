091bb9701fa7f36570bca582e2648396c64ec0fa
Vitalik <2vitalik@gmail.com>
2025-11-30 18:59:54 +0200

    web/cron: Refactor admin check handling in `cron-reset` view

--- web/main/views.py
+++ web/main/views.py
@@ -35,10 +35,12 @@ class CronListView(TemplateView):
 
 class CronResetView(TemplateView):
     def get(self, request, *args, **kwargs):
+        is_admin = 'admin' in self.request.GET
+
         if kwargs['prj'] == 'ws0':
             if not new_job_reset(kwargs['slug']):
                 ...  # todo: return render(request, 'jobs_error.html', {'error': u"Файл не найден"})
-        elif kwargs['prj'] == 'ws1' and 'admin' in self.request.GET:
+        elif kwargs['prj'] == 'ws1' and is_admin:
             if not job_reset(kwargs['slug']):
                 ...  # todo: return render(request, 'jobs_error.html', {'error': u"Файл не найден"})
         return redirect('cron-list')
